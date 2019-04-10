245a16d2bfce470e96ec89d734b02883eef44ca7
Wes McC <wkmccowin@yahoo.com>
2019-04-10 10:44:44 -0700

    updating

---
+++ assets/javascript/logic.js
@@ -0,0 +1,10 @@
+var config = {
+    apiKey: "AIzaSyBLudwMFxVNzoCnJ1l-EgLSRWWJdjWtJeQ",
+    authDomain: "lambo-tracker.firebaseapp.com",
+    databaseURL: "https://lambo-tracker.firebaseio.com",
+    projectId: "lambo-tracker",
+    storageBucket: "lambo-tracker.appspot.com",
+    messagingSenderId: "658876360231"
+  };
+  
+  firebase.initializeApp(config);
