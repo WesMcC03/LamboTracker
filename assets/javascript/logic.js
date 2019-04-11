e85ff7aebbf8a91396faa99e6d7e34cd6e96b6bc
Wes McC <wkmccowin@yahoo.com>
2019-04-10 20:11:54 -0700

    update

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -8,3 +8,36 @@ var config = {
   };
   
   firebase.initializeApp(config);
+
+  var database = firebase.database()
+
+  var lamboname = ""
+  var dest = ""
+  var exptime = ""
+  var freq = ""
+
+
+  $("#submitbutton").on("click", function() {
+
+    var lamboname = $("#lamboname").val().trim();
+    var dest = $("#destination").val().trim();
+    var exptime = $("#vesseltime").val().trim();
+    var freq = $("#frequency").val().trim();
+
+    database.ref().push({
+        modelname: lamboname,
+        destination: dest,
+        expectedtime: exptime,
+        frequency: freq
+
+    });
+  
+});
+
+database().ref().on("value",function(snap){
+    $("namedisplay").html(snap.val().lamboname);
+    $("destdisplay").html(snap.val().dest);
+    $("timedisplay").html(snap.val().exptime);
+    $("freqdisplay").html(snap.val().freq);
+})
+  
